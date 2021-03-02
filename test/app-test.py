e12efc5fb7b7dbd8c9955ac4b0fdde7f9c0c8f56
onsi <onsimini@gmail.com>
2021-03-01 21:56:01 -0500

    feat: add test - in progress

---
+++ test/app-test.py
@@ -0,0 +1,10 @@
+from website.app import app
+
+
+def test_index():
+    print(f'hello')
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello World!"
