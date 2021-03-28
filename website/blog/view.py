a793ec4c2bffa02503b875992b23f871fd89e482
onsi <onsimini@gmail.com>
2021-03-28 15:41:03 -0400

    feat: remove all

--- website/blog/view.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from website.models import Post
-
-blog = Blueprint('blog', __name__, template_folder='templates', static_folder='static')
-
-@blog.route('/')
-def index():
-    posts = Post.query.all()
-    return render_template('blog/index.html', posts=posts)
