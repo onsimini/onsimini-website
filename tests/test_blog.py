f44a7e2bbbf68c0ed919a48ad909b011413d0c06
onsi <onsimini@gmail.com>
2021-04-01 22:26:38 -0400

    test: remove a test of a removed feature

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -5,7 +5,7 @@ from website.db import get_db
 def test_index(client, auth):
     response = client.get('/')
     assert b"Log In" in response.data
-    assert b"Register" in response.data
+    # assert b"Register" in response.data
 
     auth.login()
     response = client.get('/')
