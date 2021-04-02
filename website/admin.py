83f9f243f19411c78c87ebe0694eab2b5461b178
onsi <onsimini@gmail.com>
2021-04-01 22:18:41 -0400

    feat: add adminpage with login/logout

--- website/admin.py
+++ website/admin.py
@@ -1,36 +1,52 @@
+import functools
+
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for
-    )
-from werkzeug.security import generate_password_hash
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from website.db import get_db
+
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @bp.route('/', methods=('GET', 'POST'))
 def admin():
+
+    error = None
+
+    if request.method == 'POST':
+        if request.form['logout']:
+            session.clear()
+            return redirect(url_for('admin.admin'))
+
+    if error:
+        flash(error)
+
+    return render_template('admin/index.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('admin.admin'))
 
         flash(error)
 
