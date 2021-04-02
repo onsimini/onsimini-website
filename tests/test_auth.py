6809a006d7f08d4877056575003703c975b395ba
onsi <onsimini@gmail.com>
2021-04-02 11:44:25 -0400

    fix: clean code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,4 +1,3 @@
-import pytest
 from flask import g, session
 from website.db import get_db
 
@@ -16,19 +15,6 @@ def test_register(client, app):
         ).fetchone() is not None
 
 
-# @pytest.mark.parametrize(('username', 'password', 'message'), (
-#     ('', '', b'Username is required.'),
-#     ('a', '', b'Password is required.'),
-#     ('test', 'test', b'already registered'),
-# ))
-# def test_register_validate_input(client, username, password, message):
-#     response = client.post(
-#         '/auth/register',
-#         data={'username': username, 'password': password}
-#     )
-#     assert message in response.data
-
-
 def test_login(client, auth):
     assert client.get('/auth/login').status_code == 200
     response = auth.login()
@@ -38,15 +24,6 @@ def test_login(client, auth):
         client.get('/')
         assert session['user_id'] == 1
         assert g.user['username'] == 'test'
-
-
-# @pytest.mark.parametrize(('username', 'password', 'message'), (
-#     ('a', 'test', b'Incorrect username.'),
-#     ('test', 'a', b'Incorrect password.'),
-# ))
-# def test_login_validate_input(auth, username, password, message):
-#     response = auth.login(username, password)
-#     assert message in response.data
 
 
 def test_logout(client, auth):
--- tests/test_blog.py
+++ tests/test_blog.py
@@ -2,20 +2,6 @@ import pytest
 from website.db import get_db
 
 
-# def test_index(client, auth):
-#     response = client.get('/')
-#     # assert b"Log In" in response.data
-#     # assert b"Register" in response.data
-
-#     auth.login()
-#     response = client.get('/')
-#     # assert b'Log Out' in response.data
-#     assert b'test title' in response.data
-#     # assert b'test 2018-01-01' in response.data
-#     assert b'test\nbody' in response.data
-#     assert b'href="/1/update"' in response.data
-
-
 @pytest.mark.parametrize('path', (
     '/create',
     '/1/update',
@@ -70,16 +56,6 @@ def test_update(client, auth, app):
         db = get_db()
         post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
         assert post['title'] == 'updated'
-
-
-# @pytest.mark.parametrize('path', (
-#     '/create',
-#     '/1/update',
-# ))
-# def test_create_update_validate(client, auth, path):
-#     auth.login()
-#     response = client.post(path, data={'title': '', 'body': ''})
-#     assert b'Title is required.' in response.data
 
 
 def test_delete(client, auth, app):
