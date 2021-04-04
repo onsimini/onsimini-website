61629b75442afa589be379b897b6c56925a1c8bb
onsi <onsimini@gmail.com>
2021-04-04 14:28:53 -0400

    feat: add register user field in admin page

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, flash, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from website.db import get_db
 
 
@@ -44,6 +44,36 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
+            return redirect(url_for('admin.admin'))
+
+        flash(error)
+
+    return render_template('admin/index.html')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
             return redirect(url_for('admin.admin'))
 
         flash(error)
