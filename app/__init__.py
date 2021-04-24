9eb2decb66172b3d0ae3631673cf693b5a0118ec
onsi <onsimini@gmail.com>
2021-04-23 23:54:04 -0400

    feat: add sqlite DB with basic post table

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=Post.query.all())
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
