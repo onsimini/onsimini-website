523015c51c857d354617daba1fa3ca2cdb9b64ac
onsi <onsimini@gmail.com>
2021-04-25 16:56:15 -0400

    feat: update flask to factory form

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', posts=Post.query.all())
+def create_app():
+    app = Flask(__name__)
+    db.init_app(app)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Post %r>' % self.title
-
+    return app
