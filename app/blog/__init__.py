ebb590a788cf544edebf44d811df65ffaa106f9b
onsi <onsimini@gmail.com>
2021-04-25 21:37:46 -0400

    feat: simple flask factory app blog page (need admin part)

---
+++ app/blog/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('blog',
+               __name__,
+               template_folder='templates',
+               static_folder='static'
+               )
+
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
