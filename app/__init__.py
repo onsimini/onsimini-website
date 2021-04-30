96518861667e8b452f48b14982f3cde368ffbbdc
onsi <onsimini@gmail.com>
2021-04-29 21:00:01 -0400

    feat: add error_pages bp with error 404 handler

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ def create_app():
     from . import admin
     app.register_blueprint(admin.bp)
 
+    from app.error_pages.handlers import error_pages
+    app.register_blueprint(error_pages)
+
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
