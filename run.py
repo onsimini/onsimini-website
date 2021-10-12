27a2d668ff2b3bc29bd8a84df1f40683e5d012c4
onsimini <onsimini@gmail.com>
2021-10-11 21:03:14 -0400

    feat: add basic posts

--- run.py
+++ run.py
@@ -2,14 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Gontran Sion',
+        'title': 'Blog Post 1.',
+        'content': 'First post content.',
+        'date': 'October 10, 2021'
+    },
+    {
+        'author': 'Gontran Sion',
+        'title': 'Blog Post 2.',
+        'content': 'Second post content.',
+        'date': 'October 11, 2021'
+    },
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
 
 if __name__ == '__main__':
     app.run()
